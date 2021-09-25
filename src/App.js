6499cc92fe3f74fe43cec1502c0a66259e2474ba
eslazyk10 <eslazy32@gmail.com>
2021-09-25 13:25:22 -0500

    updated useEffect issue

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ const App = () => {
   const [ query, setQuery ] = useState('chicken');
 
   useEffect(() => {
-    getRecipes();
-  }, [query]);
-
-  const getRecipes = async () => {
+    const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   }
+    getRecipes();
+  }, [query]);
+
 
   const updateSearch = e => {
     setSearch(e.target.value);
